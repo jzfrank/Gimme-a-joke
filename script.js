555d498c6eea61ad12abc088d3e49d27529207eb
jzfrank <frank.jin.zhang@outlook.com>
2022-03-13 17:09:46 +0100

    now 1/3 chance of giving a geek joke 2/3 chance of giving a dad joke

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 //jshint esversion: 8
 
-const joke_history = new Set();
+const geek_joke_history = new Set();
 
-new_joke();
+geek_joke();
 
 // $("#new-joke").on("click", new_joke);
-$("#new-joke").on("click", new_joke);
+$("#new-joke").on("click", ()=>{
+    if (Math.random() < 1/3) geek_joke();
+    else dad_joke();
+});
 
-async function new_joke()  {
+async function geek_joke()  {
   fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit')
     .then(response => response.json())
     .then(data => {
-      if (joke_history.has(data.id)) {
+      if (geek_joke_history.has(data.id)) {
         console.log("A duplicated joke", data);
-        return new_joke();
+        return geek_joke();
       }
       if (data.type === 'twopart') {
           $("#joke-content").html(data.setup + " <br><br> " + data.delivery);
@@ -21,7 +24,7 @@ async function new_joke()  {
       else {
           $("#joke-content").html(data.joke);
       }
-      joke_history.add(data.id);
+      geek_joke_history.add(data.id);
       console.log(data);
     });
 }
@@ -30,6 +33,7 @@ async function dad_joke() {
   fetch('https://icanhazdadjoke.com/slack')
    .then(response => response.json())
    .then(data => {
+     $("#joke-content").html(data.attachments[0].text);
      console.log(data);
      console.log(data.attachments[0].text);}
    );
